6ef6438e71dd492b47c15beafc3e78b663281885
Kasey Baughan <kaseyb002@yahoo.com>
2025-12-17 10:12:23 -0500

    cleanup

--- Package.swift
+++ Package.swift
@@ -11,15 +11,12 @@ let package = Package(
         .watchOS(.v6),
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "KaseyCodableStore",
             targets: ["KaseyCodableStore"]
         ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "KaseyCodableStore"
         ),
