a55d5bc31766e81bfd434e6f19c200437d31f523
Kasey Baughan <kaseyb002@yahoo.com>
2025-12-17 10:18:32 -0500

    fix definition

--- Sources/KaseyCodableStore/CodableStore.swift
+++ Sources/KaseyCodableStore/CodableStore.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 /// Stores, retrieves, and deletes a single Codable object
-public protocol CodableStore: Sendable {
+public protocol CodableStore<CodableObject>: Sendable where CodableObject: Codable & Sendable {
     associatedtype CodableObject: Codable & Sendable
 
     func save(object: CodableObject) async throws
